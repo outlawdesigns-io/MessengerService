422d15bff96382fb9a964128ce5d2656ea942580
t3kj3wlz <outlawstar4761@gmail.com>
2019-02-25 14:29:22 -0600

    adding secure endpoint functions

--- Endpoint.php
+++ Endpoint.php
@@ -4,11 +4,51 @@ require_once __DIR__ . '/Api/Api.php';
 
 class EndPoint extends API{
 
+    const ACCOUNTS = 'http://api.outlawdesigns.io:9661/';
+
     public function __construct($request,$origin)
     {
         parent::__construct($request);
     }
+    private function _verifyToken(){
+      $ch = curl_init();
+      curl_setopt($ch,CURLOPT_URL,self::ACCOUNTS . "verify/");
+      curl_setopt($ch,CURLOPT_HTTPHEADER,array('auth_token: ' . $this->headers['auth_token']));
+      curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+      $output = json_decode(curl_exec($ch));
+      curl_close($ch);
+      if(isset($output->error)){
+        return false;
+      }
+      $this->user = $output;
+      return true;
+    }
+    private function _authenticate(){
+      $headers = array('request_token: ' . $this->headers['request_token'],'password: ' . $this->headers['password']);
+      $ch = curl_init();
+      curl_setopt($ch,CURLOPT_URL,self::ACCOUNTS . "authenticate/");
+      curl_setopt($ch,CURLOPT_HTTPHEADER,$headers);
+      curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+      $output = json_decode(curl_exec($ch));
+      curl_close($ch);
+      if(isset($output->error)){
+        throw new \Exception($output->error);
+      }
+      $this->headers['auth_token'] = $output->token;
+      $this->_verifyToken();
+      return $output;
+    }
     protected function example(){
         return array("endPoint"=>$this->endpoint,"verb"=>$this->verb,"args"=>$this->args,"request"=>$this->request);
     }
+    protected function authenticate(){
+      return $this->_authenticate();
+    }
+    protected function verify(){
+      if(!$this->_verifyToken()){
+        throw new \Exception('Token Rejected');
+      }
+      return $this->headers['auth_token'];
+    }
+    protected function send(){}
 }
